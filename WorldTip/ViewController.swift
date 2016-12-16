2837fe10fc12acce6be7480f63e979d9d67d101c
Will Gilman <will@samadhi.tech>
2016-12-16 12:50:34 -0800

    Initial Commit

---
+++ WorldTip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorldTip
+//
+//  Created by Will Gilman on 12/16/16.
+//  Copyright © 2016 Will Gilman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
